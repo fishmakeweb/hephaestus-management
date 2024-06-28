a3cb6f77f6c675cc7cd990e34b69bf8ad71a99fd
PhatNLT <letienphat03@gmail.com>
2024-06-28 19:32:52 +0700

    Do css for not-found file

---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+
+export default function NotFound() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-black">
+      <div className="text-white text-center space-y-4">
+        <h1 className="text-xl font-bold">404 | Page not found.</h1>
+      </div>
+    </div>
+  );
+}

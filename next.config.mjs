87e365ecc05b2a1e96da5b5dd21009b6e31df371
PhatNLT <letienphat03@gmail.com>
2024-06-28 19:32:17 +0700

    Modify and Config some Lib/UI

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,11 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig = {
+    images: {
+        domains: [
+            'ap-south-1.linodeobjects.com', // Ensure this is your intended domain
+        ],
+    },
     output: "standalone",
     webpack: (config, { isServer }) => {
         config.resolve.alias['@'] = path.resolve(__dirname, 'src');

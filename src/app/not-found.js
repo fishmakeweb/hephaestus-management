91496d16c9ce8774cb5106b44e41846524ffdeb7
PhatNLT <letienphat03@gmail.com>
2024-07-09 16:40:25 +0700

    Change 404 CSS (temp)

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,7 +3,7 @@ export default function NotFound() {
   return (
     <div className="w-full h-screen flex items-center justify-center bg-black">
       <div className="text-white text-center space-y-4">
-        <h1 className="text-xl font-bold">404 | Page not found.</h1>
+        <h1 className="text-xl font-bold">You dont have access to view this page.</h1>
       </div>
     </div>
   );

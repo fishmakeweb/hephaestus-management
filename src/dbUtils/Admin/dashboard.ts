c403b25a15696f87cb5952981ca1d4ecbfd480b7
PhatNLT <letienphat03@gmail.com>
2024-07-09 11:33:14 +0700

    Create dashboard.ts to fetch APIS

---
+++ src/dbUtils/Admin/dashboard.ts
@@ -0,0 +1,49 @@
+
+import axios from "@/dbUtils/axios"
+import { User } from "lucide-react";
+
+export interface User {
+    userId: number;
+    fullName: string;
+    email: string;
+    address: string;
+    registeredDate: Date;
+    username: string;
+}
+
+export interface Category {
+    categoryId: number;
+    categoryName : string;
+    orderCount : number;
+    categoryImg : string;
+}
+
+export async function fetchAllCustomers(): Promise<User[]> {
+    try {
+        const response = await axios.get<User[]>('/secure/customers');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching orders:', error);
+        throw error;
+    }
+}
+
+export async function fetchCategories(): Promise<Category[]> {
+    try {
+        const response = await axios.get<Category[]>('/categories/order-count');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching orders:', error);
+        throw error;
+    }
+}
+
+export async function fetchTop1Cate(): Promise<Category> {
+    try {
+        const response = await axios.get<Category>('/categories/top-order-count');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching orders:', error);
+        throw error;
+    }
+}

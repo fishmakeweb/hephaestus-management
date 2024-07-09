3ae89c0c669652761f046c43385168c8adb67b1a
= <phungnhatan2204@gmail.com>
2024-07-09 20:23:55 +0700

    Secure for add product

--- src/dbUtils/Admin/AddProduct.ts
+++ src/dbUtils/Admin/AddProduct.ts
@@ -1,123 +1,162 @@
-import axios from '@/dbUtils/axios';
+import axios from "@/dbUtils/axios";
 
 class AddProductUtils {
-
-    async fetchAllDiamondAtribute() {
-        try {
-            const response = await axios.get("/diamonds/all");
-            return response;
-        } catch (error) {
-            console.error('Error fetching diamonds: ', error);
-        }
+  async fetchAllDiamondAtribute() {
+    try {
+      // SECURE DONE
+      const response = await axios.get("/diamonds/all", {
+        headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` },
+      });
+      return response;
+    } catch (error) {
+      console.error("Error fetching diamonds: ", error);
     }
+  }
 
-    async handleFileChange(event: any) {
-        const file = event.target.files[0];
-        const formData = new FormData();
-        formData.append("file", file);
-        try {
-            const response = await axios.post("/upload", formData, {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                },
-            });
+  // SECURE DONE
+  async handleFileChange(event: any) {
+    const file = event.target.files[0];
+    const formData = new FormData();
+    formData.append("file", file);
+    try {
+      const response = await axios.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${sessionStorage.getItem("token")}}`,
+        },
+      });
 
-            const imageUrl = response.data; 
-            return imageUrl;
-        } catch (error) {
-            console.error("Error uploading file: ", error);
-        }
+      const imageUrl = response.data;
+      return imageUrl;
+    } catch (error) {
+      console.error("Error uploading file: ", error);
     }
+  }
 
-    async saveDiamondToDB( measurement: any, color: any, cut: any, carat: any, clarity: any, gia: any, price: any, img: any) {
-        const diamondData = {
-            measurement: { measurementId: measurement },
-            color: { colorId: color },
-            cut: { cutId: cut },
-            carat: { caratId: carat },
-            clarity: { clarityId: clarity },
-            gia: { issueDate: gia },
-            price: price,
-            img: img,
-        };
-        try {
-            const response = await axios.post("/secure/diamonds", diamondData);
-            console.log('Save diamond successfull: ', response);
-            return true;
-        } catch (error) {
-            console.error("Add diamond failed:", error);
-            return false;
-        }
+  // SECURE DONE
+  async saveDiamondToDB(
+    measurement: any,
+    color: any,
+    cut: any,
+    carat: any,
+    clarity: any,
+    gia: any,
+    price: any,
+    img: any
+  ) {
+    const diamondData = {
+      measurement: { measurementId: measurement },
+      color: { colorId: color },
+      cut: { cutId: cut },
+      carat: { caratId: carat },
+      clarity: { clarityId: clarity },
+      gia: { issueDate: gia },
+      price: price,
+      img: img,
     };
-
-    async updateDiamondStatus(diamondId:any){
-        try {
-            const response = await axios.put(`/secure/set/diamonds/${diamondId}`);
-            console.log('Update diamond status successfull: ', response);
-            return true;
-        } catch (error) {
-            console.error("Update diamond status failed:", error);
-            return false;
-        }
+    try {
+      const response = await axios.post("/secure/diamonds", diamondData, {
+        headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` },
+      });
+      console.log("Save diamond successfull: ", response);
+      return true;
+    } catch (error) {
+      console.error("Add diamond failed:", error);
+      return false;
     }
-    async fetchAllJewelryAtribute() {
-        try {
-            const response = await axios.get("/jewelry/all");
-            return response;
-        } catch (error) {
-            console.error('Error fetching jewelry attributes: ', error);
-        }
-    }
+  }
 
-    async fetchAllDiamonds() {
-        try {
-            const response = await axios.get("/diamonds");
-            return response;
-        } catch (error) {
-            console.error('Error fetching jewelry attributes: ', error);
-        }
+  //SECURE DONE
+  async updateDiamondStatus(diamondId: any) {
+    try {
+      const response = await axios.put(`/secure/set/diamonds/${diamondId}`, {
+        headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` },
+      });
+      console.log("Update diamond status successfull: ", response);
+      return true;
+    } catch (error) {
+      console.error("Update diamond status failed:", error);
+      return false;
     }
+  }
 
-    async jewelryUniqueName(jewelryName: any) {
-        try {
-            const response = await axios.get(`/jewelry/check-name/${jewelryName}`);
-            return response;
-        } catch (error) {
-            console.error("Error checking jewelry name:", error);
-        }
+  // SECURE DONE
+  async fetchAllJewelryAtribute() {
+    try {
+      const response = await axios.get("/jewelry/all", {
+        headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` },
+      });
+      return response;
+    } catch (error) {
+      console.error("Error fetching jewelry attributes: ", error);
     }
+  }
 
-    async saveJewelryToDB(jewelryName: any, jewelryUrl: any, jewelryPrice: any, jewelryQuantity: any, selectedMaterial: any, selectedCategory: any, selectedSize: any, selectedDiamond: any, selectedShape:any) {
-        const jewelryData = {
-            name: jewelryName,
-            img: jewelryUrl,
-            price: jewelryPrice,
-            quantity: jewelryQuantity,
-            date: new Date(), // Add the current date
-            material: {
-                materialId: selectedMaterial,
-            },
-            shape: {
-                shapeId: selectedShape,
-            },
-            category: {
-                categoryId: selectedCategory,
-            },
-            size: {
-                sizeId: selectedSize,
-            },
-            diamond: selectedDiamond ? { diamondId: selectedDiamond } : null,
-        };
-        try {
-            const response = await axios.post("/secure/jewelry", jewelryData);
-            console.log('Save jewelry successfull: ', response);
-            return true;
-        } catch (error) {
-            console.error("Add jewelry failed:", error);
-        }
+  // SECURE DONE
+  async fetchAllDiamonds() {
+    try {
+      const response = await axios.get("/diamonds", {
+        headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` },
+      });
+      return response;
+    } catch (error) {
+      console.error("Error fetching jewelry attributes: ", error);
     }
-    
+  }
 
+  // NO SECURE
+  async jewelryUniqueName(jewelryName: any) {
+    try {
+      const response = await axios.get(`/jewelry/check-name/${jewelryName}`);
+      return response;
+    } catch (error) {
+      console.error("Error checking jewelry name:", error);
+    }
+  }
+
+
+  // SECURE DONE
+  async saveJewelryToDB(
+    jewelryName: any,
+    jewelryUrl: any,
+    jewelryPrice: any,
+    jewelryQuantity: any,
+    selectedMaterial: any,
+    selectedCategory: any,
+    selectedSize: any,
+    selectedDiamond: any,
+    selectedShape: any
+  ) {
+    const jewelryData = {
+      name: jewelryName,
+      img: jewelryUrl,
+      price: jewelryPrice,
+      quantity: jewelryQuantity,
+      date: new Date(), // Add the current date
+      material: {
+        materialId: selectedMaterial,
+      },
+      shape: {
+        shapeId: selectedShape,
+      },
+      category: {
+        categoryId: selectedCategory,
+      },
+      size: {
+        sizeId: selectedSize,
+      },
+      diamond: selectedDiamond ? { diamondId: selectedDiamond } : null,
+    };
+    try {
+      const response = await axios.post("/secure/jewelry", jewelryData, {
+        headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` },
+      });
+      console.log("Save jewelry successfull: ", response);
+      return true;
+    } catch (error) {
+      console.error("Add jewelry failed:", error);
+    }
+  }
 }
 
 export default AddProductUtils;

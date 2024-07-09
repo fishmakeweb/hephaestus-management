f6b19dd03bec4a9547b3f38bd24f1f26aab10767
= <phungnhatan2204@gmail.com>
2024-07-09 20:26:00 +0700

    Secure for dashboard

--- src/dbUtils/Admin/dashboard.ts
+++ src/dbUtils/Admin/dashboard.ts
@@ -1,49 +1,57 @@
-
-import axios from "@/dbUtils/axios"
+import axios from "@/dbUtils/axios";
 import { User } from "lucide-react";
 
 export interface User {
-    userId: number;
-    fullName: string;
-    email: string;
-    address: string;
-    registeredDate: Date;
-    username: string;
+  userId: number;
+  fullName: string;
+  email: string;
+  address: string;
+  registeredDate: Date;
+  username: string;
 }
 
 export interface Category {
-    categoryId: number;
-    categoryName : string;
-    orderCount : number;
-    categoryImg : string;
+  categoryId: number;
+  categoryName: string;
+  orderCount: number;
+  categoryImg: string;
 }
 
+// SECURE DONE
 export async function fetchAllCustomers(): Promise<User[]> {
-    try {
-        const response = await axios.get<User[]>('/secure/customers');
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching orders:', error);
-        throw error;
-    }
+  try {
+    const response = await axios.get<User[]>("/secure/customers", {
+      headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching orders:", error);
+    throw error;
+  }
 }
 
+
+// SECURE NOT DONE
 export async function fetchCategories(): Promise<Category[]> {
-    try {
-        const response = await axios.get<Category[]>('/categories/order-count');
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching orders:', error);
-        throw error;
-    }
+  try {
+    const response = await axios.get<Category[]>("/categories/order-count", {
+      headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching orders:", error);
+    throw error;
+  }
 }
 
+
+// SECURE NOT DONE
 export async function fetchTop1Cate(): Promise<Category> {
-    try {
-        const response = await axios.get<Category>('/categories/top-order-count');
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching orders:', error);
-        throw error;
-    }
+  try {
+    const response = await axios.get<Category>("/categories/top-order-count");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching orders:", error);
+    throw error;
+  }
 }

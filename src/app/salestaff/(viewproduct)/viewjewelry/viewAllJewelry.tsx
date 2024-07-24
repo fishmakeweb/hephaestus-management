1498802f69332ea75cd753d930f7498b6f648b4d
PhatNLT <letienphat03@gmail.com>
2024-07-24 21:49:51 +0700

    Remove to fixed

--- src/app/salestaff/(viewproduct)/viewjewelry/viewAllJewelry.tsx
+++ src/app/salestaff/(viewproduct)/viewjewelry/viewAllJewelry.tsx
@@ -234,7 +234,7 @@ const ViewAllJewelry: React.FC = () => {
                       {item.size.sizeNumber} {item.size.unit}
                     </td>
                     <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">
-                      {item.price.toFixed(2)} VNĐ
+                      {item.price} VNĐ
                     </td>
                     <td className="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">
                       {item.quantity}
